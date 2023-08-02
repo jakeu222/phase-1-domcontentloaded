0ee8a89f64ed65a1c5424493d0c7fead3bb46121
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-08-02 14:57:12 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    yourDomHasBeenUpdated()
+
+})
+function yourDomHasBeenUpdated() {
+    document.querySelector("#text").textContent = "This is really cool!"
+}
+
